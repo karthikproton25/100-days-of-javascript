0cfbae5c2c93ca3e7403be4abc5a655fda42a181
Karthik <karthik2502@protonmail.com>
2024-10-06 22:54:52 +0530

    adding js to the two sides scroll project

--- two-sides-scroll/app.js
+++ two-sides-scroll/app.js
@@ -1,41 +1,35 @@
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Two Side Scroll</title>
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-    <section class="slider-container">
-      <section class="left-slid">
-        <div class="game-one">
-          <h1>God Of War</h1>
-        </div>
-        <div class="game-two">
-          <h1>The Witcher</h1>
-        </div>
-        <div class="game-three">
-          <h1>Watch Dogs 2</h1>
-        </div>
-        <div class="game-four">
-          <h1>Assassin's Creed</h1>
-        </div>
-      </section>
+const sliderContainer = document.querySelector(".slider-container");
+const slidRight = document.querySelector(".right-slid");
+const slidLeft = document.querySelector(".left-slid");
+const upButton = document.getElementById("up-btn");
+const downButton = document.getElementById("down-btn");
+const sliderLength = slidRight.querySelectorAll("div").length;
 
-      <section class="right-slid">
-        <div class="img img-one"></div>
-        <div class="img img-two"></div>
-        <div class="img img-three"></div>
-        <div class="img img-four"></div>
-      </section>
+let activeSlideIndex = 0;
+slidLeft.style.top = `-${(sliderLength - 1) * 100}vh`;
 
-      <section class="buttons-container">
-        <button id="down-btn">&#8595;</button>
-        <button id="up-btn">&#8593;</button>
-      </section>
-    </section>
+downButton.addEventListener("click", () => nextSlide("up"));
+upButton.addEventListener("click", () => nextSlide("down"));
 
-    <script src="app.js"></script>
-  </body>
-</html>;
+function nextSlide(params) {
+  const sliderHeight = sliderContainer.clientHeight;
+
+  if (params === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > sliderLength - 1) {
+      activeSlideIndex = 0;
+    }
+  }
+
+  if (params === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = sliderLength - 1;
+    }
+  }
+
+  slidRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+  slidLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
+}
